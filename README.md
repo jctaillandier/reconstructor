dc7b638428773f89b16a89c0b3b7fc6d732bb38a
Jean-Christophe Taillandier <jean-christophe.taillandier@umontreal.ca>
2020-02-23 14:49:59 -0500

    Hp tuning

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 - Using Auto Encoder
 
-- Loss between autoencoder output and original image
+- Currently implemented 
+
+- Loss used is L1 distance between autoencoder output and original image (for both test and train)
+
+- If possible, test set will pass as one batch, and in last epoch, will save generated images in Experiment folder
+
+
--- launch_reconst_gpu.sh
+++ launch_reconst_gpu.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=def-gambsseb
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem=120000M
-#SBATCH --time=24:00:00
-#SBATCH --job-name=100_epochs_a=0
+#SBATCH --mem=50000M
+#SBATCH --time=06:00:00
+#SBATCH --job-name=100_epochs_a0
 #SBATCH --mail-user=jean-christophe.taillandier@umontreal.ca
 #SBATCH --output=./%x-%j.out
 #SBATCH --mail-type=ALL
 source ~/gpu_ready_pyenv/bin/activate
-python3 ./reconstructor/reconstructor.py params.yaml '100ep_a=0'
+python3 ./reconstructor.py params.yaml '100_epochs_a0'
