ee5f6dcf75351556c88fccb5aeb2f1523f27b547
Jean-Christophe Taillandier <jct@cedar5.cedar.computecanada.ca>
2020-03-06 09:56:54 -0800

    launch.sh updated

--- launch_reconst_cpu.sh
+++ launch_reconst_cpu.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-gambsseb
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
 #SBATCH --mem=50000M
 #SBATCH --time=06:00:00
-#SBATCH --job-name=adult
+#SBATCH --job-name=adult_2048
 #SBATCH --mail-user=jean-christophe.taillandier@umontreal.ca
 #SBATCH --output=./%x-%j.out
 #SBATCH --mail-type=ALL
 source ~/gpu_ready_pyenv/bin/activate
-python3 ./reconstructor.py -n adult -bs 1024 -ep 5000 -dr='tsne'
+python3 ./reconstructor.py -n adult -bs 2048 -ep 5000 -dr='tsne'
