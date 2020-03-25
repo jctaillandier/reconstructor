d8eb82fc801f54372b0980508d8d511594a0820b
JC <jc.taillandier@hotmail.com>
2020-03-25 07:48:29 -0400

    a

--- grid_search.py
+++ grid_search.py
@@ -3,7 +3,7 @@ import argparse, os
 from joblib import Parallel, delayed
 
 def parse_arguments(parser):
-    parser.add_argument('-in','--input_dataset', type=str, default='gansan', help='Dataset to use as input. Currently support `gansan` and `disp_impact`', required=True, choices=['gansan', 'disp_impact'])
+    parser.add_argument('-in','--input_dataset', type=str, default='gansan', help='Dataset to use as input. Currently support `gansan` and `disp_impact`', required=False, choices=['gansan', 'disp_impact'])
     parser.add_argument('-cpu','--cpu_parallel', type=int, default=1, help='How many cpu to run in parallel to accelerate trainings', required=False)
     parser.add_argument('-a','--alpha', type=float, default=0.9875, help='Value of alpha used when  sanitizing the dataset we use as input.', required=False, choices=[0,0.25,0.8,0.9875])
     parser.add_argument('-ep','--epochs', type=int, help='Number of epochs to train the model.', required=True)
